0a106a72247c72c42a7bfa23aeab8ef03aaa9910
mike-kob <37714867+mike-kob@users.noreply.github.com>
2020-01-12 21:03:55 +0200

    Add tests for init

--- initialization.py
+++ initialization.py
@@ -5,13 +5,14 @@ def uniform(num_ind, num_locuses):
     return np.random.randint(0, 2, (num_ind, num_locuses), dtype=np.int8)
 
 
-def split_population(self, pure_p, impure_p):
+def split_population(num_ind, num_locuses, pure_p, impure_p):
     assert pure_p + impure_p == 100, "Pure and impure individuals" \
                                  " together make 100% of population "
-    impure_n = round(impure_p / 100.0 * self.num_ind)
-    pure_n = self.num_ind - impure_n
-    impure = np.random.randint(2, size=(impure_n, self.num_locuses), dtype=np.int8)
-    pure = np.zeros((pure_n, self.num_locuses), dtype=np.int8)
+    impure_n = round(impure_p / 100.0 * num_ind)
+    pure_n = num_ind - impure_n
+
+    impure = np.random.randint(2, size=(impure_n, num_locuses), dtype=np.int8)
+    pure = np.zeros((pure_n, num_locuses), dtype=np.int8)
 
     res = np.concatenate([impure, pure])
     np.random.shuffle(res)
