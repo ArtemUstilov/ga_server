bdd69fcd0ad3ac60d1f88519562474c1942d3e77
mike-kob <37714867+mike-kob@users.noreply.github.com>
2020-01-19 12:26:08 +0200

    Change mutation

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from selection import roulette
 
 if __name__ == '__main__':
     # Global parameters
-    l = 100
-    N = 1000
+    l = 8000
+    N = 80000
     px = Decimal(1) / Decimal(10 * l)
     print(px)
 
     # 1. Initialization
     pop1 = uniform(N, l)
 
-    for _ in range(10000):
+    for it in range(10000):
+        print('it1:', it)
         # 2. Estimation
         health = hamming_distance(pop1)
         # print(pop1)
-        print(health.mean())
 
         # 3. Selection
-        pop1 = roulette(pop1, health, N)
+        # pop1 = roulette(pop1, health, N)
+        print('it2:', it)
 
         # 4. Mutation
         pop1 = mutate(pop1, px)
-        print()
+        print('it3:', it)
--- mutation.py
+++ mutation.py
@@ -1,12 +1,6 @@
-from decimal import Decimal
-
 import numpy as np
 
 
-def mutate(population: np.ndarray, px: Decimal):
-    mutation_matrix = np.random.choice(
-        a=2,
-        size=population.shape,
-        p=[Decimal('1')-px, px],
-    ).astype(population.dtype)
-    return np.bitwise_xor(population, mutation_matrix)
+def mutate(population: np.ndarray, px: float):
+    population[np.random.rand(*population.shape) < px] ^= 1
+    return population
